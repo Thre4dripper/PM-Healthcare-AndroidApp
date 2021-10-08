f4b2b1baeb5d135c34497f0f930284119e1cc6e2
Ijlal Ahmad <ijlalahmad845@gmail.com>
2021-10-08 23:09:55 +0530

    Added Firebase Auth with phone number by Firebase AuthUI

--- app/src/main/java/com/example/pmhealthcare/Activities/MainActivity.java
+++ app/src/main/java/com/example/pmhealthcare/Activities/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.pmhealthcare.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.pmhealthcare.Fragments.DigidocFragment;
 import com.example.pmhealthcare.Fragments.HFRFragment;
@@ -13,8 +16,15 @@ import com.example.pmhealthcare.Fragments.ProfileFragment;
 import com.example.pmhealthcare.Fragments.RecordsFragment;
 import com.example.pmhealthcare.Fragments.TeleMedicFragment;
 import com.example.pmhealthcare.R;
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +41,41 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setSelectedItemId(R.id.profile);
 
         InitFragments();
+        AUTH();
+       // System.out.println(FirebaseAuth.getInstance().getCurrentUser().getUid());
+    }
+
+    public void AUTH(){
+
+        List<AuthUI.IdpConfig> providers= Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build()
+        );
+        if(FirebaseAuth.getInstance().getCurrentUser()==null){
+            Intent intent=AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build();
+
+            startActivityForResult(intent,100);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode==100){
+            FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+            System.out.println(user.getUid());
+            if(user.getMetadata().getCreationTimestamp()==user.getMetadata().getLastSignInTimestamp())
+            Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, "Welcome Back", Toast.LENGTH_SHORT).show();
+        }
+        else {
+           // Toast.makeText(this, "not logged in", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     public void InitFragments(){
--- app/src/main/java/com/example/pmhealthcare/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/pmhealthcare/Fragments/ProfileFragment.java
@@ -7,12 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.pmhealthcare.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class ProfileFragment extends Fragment {
 
+    Button signOutBtn;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -21,6 +26,16 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view=inflater.inflate(R.layout.fragment_profile, container, false);
+        signOutBtn=view.findViewById(R.id.sign_out_button);
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance().signOut(getActivity());
+                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
