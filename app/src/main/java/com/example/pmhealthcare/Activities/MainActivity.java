3786b8b105293db0790b26067f56c9195004c9f4
Ijlal Ahmad <ijlalahmad845@gmail.com>
2021-10-10 00:21:48 +0530

    Parsed States and Districts info from JSON and Isolated firebase auth

--- app/src/main/java/com/example/pmhealthcare/Activities/MainActivity.java
+++ app/src/main/java/com/example/pmhealthcare/Activities/MainActivity.java
@@ -1,105 +1,89 @@
 package com.example.pmhealthcare.Activities;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.pmhealthcare.Fragments.DigidocFragment;
 import com.example.pmhealthcare.Fragments.HFRFragment;
 import com.example.pmhealthcare.Fragments.ProfileFragment;
 import com.example.pmhealthcare.Fragments.RecordsFragment;
 import com.example.pmhealthcare.Fragments.TeleMedicFragment;
+import com.example.pmhealthcare.Networking.Firebase;
 import com.example.pmhealthcare.R;
-import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new ProfileFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new ProfileFragment()).commit();
 
         bottomNavigationView.setSelectedItemId(R.id.profile);
 
         InitFragments();
-        //AUTH();
+        AUTH();
     }
 
-    public void AUTH(){
+    /**====================================== METHOD FOR TRIGGERING FIREBASE AUTH ===================================**/
+    public void AUTH() {
 
-        List<AuthUI.IdpConfig> providers= Arrays.asList(
-                new AuthUI.IdpConfig.PhoneBuilder().build()
-        );
-        if(FirebaseAuth.getInstance().getCurrentUser()==null){
-            Intent intent=AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setAvailableProviders(providers)
-                    .build();
+        Intent intent = Firebase.LoadFirebaseAUTHUI();
+        if (intent != null)
+            startActivityForResult(intent, Firebase.FIREBASE_REQUEST_CODE);
 
-            startActivityForResult(intent,100);
-        }
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_CANCELED && requestCode==100){
+        if(!Firebase.FireBaseAUTHUIRESULT(this,resultCode,requestCode))
             finish();
-        }
-        else {
-            FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
-            System.out.println(user.getUid());
-            if(user.getMetadata().getCreationTimestamp()==user.getMetadata().getLastSignInTimestamp())
-                Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show();
-            else
-                Toast.makeText(this, "Welcome Back", Toast.LENGTH_SHORT).show();
-        }
+
     }
 
-    public void InitFragments(){
+    public void InitFragments() {
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                Fragment fragment=null;
-                if(item.getItemId()==R.id.hfr)
-                    fragment=new HFRFragment();
+                Fragment fragment = null;
+                if (item.getItemId() == R.id.hfr)
+                    fragment = new HFRFragment();
 
-                else if(item.getItemId()==R.id.digiDoc)
-                    fragment=new DigidocFragment();
+                else if (item.getItemId() == R.id.digiDoc)
+                    fragment = new DigidocFragment();
 
-                else if(item.getItemId()==R.id.records)
-                    fragment=new RecordsFragment();
+                else if (item.getItemId() == R.id.records)
+                    fragment = new RecordsFragment();
 
-                else if(item.getItemId()==R.id.tele)
-                    fragment=new TeleMedicFragment();
+                else if (item.getItemId() == R.id.tele)
+                    fragment = new TeleMedicFragment();
 
-                else if(item.getItemId()==R.id.profile)
-                    fragment=new ProfileFragment();
+                else if (item.getItemId() == R.id.profile)
+                    fragment = new ProfileFragment();
 
 
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
                 return true;
             }
         });
--- app/src/main/java/com/example/pmhealthcare/Utils/JsonParser.java
+++ app/src/main/java/com/example/pmhealthcare/Utils/JsonParser.java
@@ -2,10 +2,9 @@ package com.example.pmhealthcare.Utils;
 
 import android.content.Context;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -16,9 +15,8 @@ public class JsonParser {
 
     static String STATES_DISTRICTS_JSON=null;
 
-    public static String[] getStates(Context context){
+    public static String[] getStatesFromJSON(Context context){
         loadJSONFromAsset(context);
-
         List<String> list=new ArrayList<>();
 
         try {
@@ -27,6 +25,7 @@ public class JsonParser {
             for(int i=0;i<36;i++){
                 list.add(rootObject.getJSONArray("states_and_districts").getJSONObject(i).getString("name"));
             }
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -36,7 +35,7 @@ public class JsonParser {
         return states;
     }
 
-    public static String[] getDistricts(Context context,String currentState){
+    public static String[] getDistrictsFromJSON(Context context, String currentState){
         loadJSONFromAsset(context);
 
         List<String> list=new ArrayList<>();
@@ -45,23 +44,28 @@ public class JsonParser {
             JSONObject rootObject=new JSONObject(STATES_DISTRICTS_JSON);
 
             int i=0;
-            while(!rootObject.getJSONArray("states_and_districts").getJSONObject(i).getString("name").equals(currentState)){
-                i++;
-            }
+
             JSONObject state=rootObject.getJSONArray("states_and_districts").getJSONObject(i);
-            for(int j=0;j<i;j++){
-                list.add(state.getJSONArray("districts").getString(j));
+            while(!state.getString("name").equals(currentState)){
+                i++;
+                state=rootObject.getJSONArray("states_and_districts").getJSONObject(i);
+            }
+
+            JSONArray districts=state.getJSONArray("districts");
+            for(int j=0;j<districts.length();j++){
+                list.add(districts.getString(j));
             }
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        String[] districts=new String[list.size()];
-        districts=list.toArray(districts);
-        return districts;
+        String[] districtsList=new String[list.size()];
+        districtsList=list.toArray(districtsList);
+        return districtsList;
     }
 
+    /**=========================== LOCAL JSON FILE READER =========================================**/
     public static void loadJSONFromAsset(Context context) {
         InputStream inputStream;
         try {
@@ -76,4 +80,5 @@ public class JsonParser {
             e.printStackTrace();
         }
     }
+
 }
