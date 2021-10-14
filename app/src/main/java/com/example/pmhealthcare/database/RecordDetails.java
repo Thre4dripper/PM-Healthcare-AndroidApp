5be3fa3313f4934ebdfc322cc435ac536599fc2e
Ijlal Ahmad <ijlalahmad845@gmail.com>
2021-10-14 19:25:33 +0530

    tested uploading and retrieving nested map data using data model class in FireStore

---
+++ app/src/main/java/com/example/pmhealthcare/database/RecordDetails.java
@@ -0,0 +1,39 @@
+package com.example.pmhealthcare.database;
+
+import android.net.Uri;
+
+public class RecordDetails {
+    String name="New Record";
+    String imageID;
+    int type;
+
+    public RecordDetails(String name, String imageID, int type) {
+        this.name = name;
+        this.imageID = imageID;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(String imageID) {
+        this.imageID = imageID;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
