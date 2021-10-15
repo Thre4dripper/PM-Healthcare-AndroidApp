4569593477a72bfe5089cb9b4f387d80c2629f28
Ijlal Ahmad <ijlalahmad845@gmail.com>
2021-10-15 12:02:37 +0530

    Added Offline support in Profile DP and Records Section

--- app/src/main/java/com/example/pmhealthcare/Adapters/RecordsRecyclerAdapter.java
+++ app/src/main/java/com/example/pmhealthcare/Adapters/RecordsRecyclerAdapter.java
@@ -1,17 +1,29 @@
 package com.example.pmhealthcare.Adapters;
 
+import static android.content.Context.CONNECTIVITY_SERVICE;
+
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.pmhealthcare.R;
 import com.example.pmhealthcare.database.RecordDetails;
 
@@ -22,6 +34,7 @@ public class RecordsRecyclerAdapter extends RecyclerView.Adapter<RecordsRecycler
     Context mContext;
     public static List<RecordDetails> list;
 
+    ProgressDialog progressDialog;
 
     public interface RecordsItemOnClickInterface{
         void onRecordsItemOnCLick(int position, Uri imageUri);
@@ -34,6 +47,23 @@ public class RecordsRecyclerAdapter extends RecyclerView.Adapter<RecordsRecycler
         mContext=context;
         RecordsRecyclerAdapter.list =list;
         recordsItemOnClickInterface=onClickInterface;
+
+        progressDialog=new ProgressDialog(context);
+
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        //checking Internet Connection for instantly showing progress dialog
+        if (!isConnected){
+            Toast.makeText(context, "No Records Available", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Or Check your Internet Connection", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            progressDialog.setTitle("Loading Records...");
+            progressDialog.show();
+            progressDialog.setCancelable(false);
+        }
     }
 
 
@@ -48,9 +78,34 @@ public class RecordsRecyclerAdapter extends RecyclerView.Adapter<RecordsRecycler
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        Glide.with(mContext).load(list.get(position).getImageID()).into(holder.recordsView);
+        //checking internet connectivity
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        //dismissing progress dialog when either no internet or records are loaded
+        if (isConnected)
+        {
+            Glide.with(mContext).load(list.get(position).getImageID()).addListener(new RequestListener<Drawable>() {
+                @Override
+                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                    progressDialog.dismiss();
+                    return false;
+                }
+
+                @Override
+                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                    progressDialog.dismiss();
+                    return false;
+                }
+            }).into(holder.recordsView);
+        }
+        else
+            progressDialog.dismiss();
+
+
         holder.textView.setText(list.get(position).getName());
-        //System.out.println(list.get(position).getName());
+
     }
 
     @Override
--- app/src/main/java/com/example/pmhealthcare/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/pmhealthcare/Fragments/ProfileFragment.java
@@ -1,8 +1,13 @@
 package com.example.pmhealthcare.Fragments;
 
 import static android.app.Activity.RESULT_OK;
+import static android.content.Context.CONNECTIVITY_SERVICE;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -18,6 +23,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.pmhealthcare.Activities.ProfileActivity;
 import com.example.pmhealthcare.Networking.Firebase;
 import com.example.pmhealthcare.R;
@@ -150,15 +159,46 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         StorageReference storageReference=firebaseStorage.getReference("users/"+Firebase.UNIQUE_HEALTH_ID);
 
         StorageReference fileRef=storageReference.child("userDp");
-        fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
 
-                //User.setUserDp(getContext(),uri.toString());
-                Glide.with(getContext()).load(uri).into(userDp);
-                userDpUri=uri;
-            }
-        });
+        //made a non cancellable progress dialog
+        ProgressDialog progressDialog=new ProgressDialog(getContext());
+        progressDialog.setTitle("Please Wait...");
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+
+        //checking internet connectivity
+        ConnectivityManager cm = (ConnectivityManager) getContext().getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        if(isConnected)
+        {
+            fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+
+                    Glide.with(getContext()).load(uri).addListener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            progressDialog.dismiss();
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                           progressDialog.dismiss();
+                            return false;
+                        }
+                    }).into(userDp);
+                    userDpUri=uri;
+                }
+            });
+        }
+        else {
+            Toast.makeText(getContext(),"Offline Mode Enabled",Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
+        }
+
 
     }
 
